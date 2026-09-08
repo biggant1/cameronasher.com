447e58d9cb9c4cc0af1f921efa8fc864924fd573
Cameron <camasher22@gmail.com>
2026-09-08 00:27:17 -0500

    unbreak images on cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://cameronasher.com",
+    adapter: cloudflare({
+        imageService: "compile",
+    }),
     vite: {
         plugins: [tailwindcss()],
     },
